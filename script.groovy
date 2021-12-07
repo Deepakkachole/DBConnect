aa972e40c4b9b96678cb5c3362566d9e597f6559
Deepakkachole <deepakkachole@yahoo.com>
2021-12-07 12:34:47 +0000

    Groovy Script

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp(){
+echo "Building the application in TEST"  
+}
+
+def testApp(){
+echo "Testing the application in TEST"  
+}
+def deployApp(){
+echo "Deploting the application in TEST"  
+}
+
+
+
